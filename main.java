878fbad72fbf0719eb538e6bcb3ddf1012d24ed8
Bautista Bagnarol <81656547+BautistaBag@users.noreply.github.com>
2021-11-14 20:29:09 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+package tiendaDeLibros;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		producto  CosasLindas = new producto("Programacion con java",1520,899,true);
+		
+		System.out.println(CosasLindas.getNombre());
+		System.out.println(CosasLindas.getId());
+		System.out.println(CosasLindas.getPrecio());
+		System.out.println(CosasLindas.getStock());
+		
+		
+	}
+}
